b89271e06c8f92c16e364cd5f340f6ead17daed7
Arpit Singh <singharpit0911@gmail.com>
2025-09-05 15:19:27 +0530

    landing page UI setup complete

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
+
+export default function LandingPage() {
+  return (
+    <div className="landing-page-container">
+      <nav>
+        <div className="nav-header">
+          <h2>CallBuddy</h2>
+        </div>
+        <div className="nav-list">
+          <p>Join as Guest</p>
+          <p>Register</p>
+          <button className="p-btn">Login</button>
+        </div>
+      </nav>
+      <div className="landing-main-container">
+        <div>
+          <h1>
+            <span style={{ color: "#FF9839" }}>Connect</span> with your Loved
+            Ones
+          </h1>
+          <p>Cover a distance by Call-Buddy</p>
+          <Link to="/home">
+            <button className="p-btn">Get Started</button>
+          </Link>
+        </div>
+        <div>
+          <img src="/mobile.png" alt="mobile" />
+        </div>
+      </div>
+    </div>
+  );
+}
